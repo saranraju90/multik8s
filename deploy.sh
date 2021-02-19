08b095d7b669031268265b1060d9fa6863391441
saranraju90 <saranraju90@gmail.com>
2021-02-19 15:30:56 +0100

    travis and deply files

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t saranraju90/multi-client:latest -t saranraju90/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t saranraju90/multi-server:latest -t saranraju90/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t saranraju90/multi-worker:latest -t saranraju90/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push saranraju90/multi-client:latest
+docker push saranraju90/multi-worker:latest
+docker push saranraju90/multi-server:latest
+
+docker push saranraju90/multi-client:$SHA
+docker push saranraju90/multi-worker:$SHA
+docker push saranraju90/multi-server:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployment/server-deployment server=saranraju90/multi-server:$SHA
+kubectl set image deployment/client-deployment client=saranraju90/multi-client:$SHA
+kubectl set image deployment/worker-deployment worker=saranraju90/multi-worker:$SHA
